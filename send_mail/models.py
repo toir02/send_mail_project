f3735e2e8022bac3e29935e4ef98057cd3a4d432
toir02 <wend8765@gmail.com>
2023-08-29 00:47:00 +0300

    added model Client

--- send_mail/models.py
+++ send_mail/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True,
+            'null': True}
+
+
+class Client(models.Model):
+    email = models.CharField(max_length=200, verbose_name='почта')
+    full_name = models.CharField(max_length=150, verbose_name='ФИО')
+    comment = models.TextField(verbose_name='комментарий')
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+        ordering = ('full_name',)
