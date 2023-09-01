fa6a8a04860c059bfe60fd849d90d02a0fb8908f
toir02 <wend8765@gmail.com>
2023-09-01 12:52:00 +0300

    added cbv to create client

--- send_mail/views.py
+++ send_mail/views.py
@@ -1,7 +1,14 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from send_mail.models import Client
 
 
 class ClientListView(ListView):
     model = Client
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ('full_name', 'email', 'comment')
+    success_url = reverse_lazy('send_mail:index')
