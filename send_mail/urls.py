e5a43bf9b5fa7aa33080f5a396004ea7ce66bf5a
toir02 <wend8765@gmail.com>
2023-09-01 21:33:04 +0300

    refactor imports and added path to urlpatterns

--- send_mail/urls.py
+++ send_mail/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from send_mail.apps import SendMailConfig
 
-from views import *
+from send_mail.views import *
 
 app_name = SendMailConfig.name
 
 urlpatterns = [
-    path('', ClientListView.as_view(), name='index')
+    path('', ClientListView.as_view(), name='index'),
+    path('client/create/', ClientCreateView.as_view(), name='create_client'),
+    path('client/view/<int:pk>', ClientDetailView.as_view(), name='view_client'),
+    path('client/')
 ]
