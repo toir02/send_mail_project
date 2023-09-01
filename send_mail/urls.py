2e7f5700e57adabf80d3c8692c5ca7c9ed86e69c
toir02 <wend8765@gmail.com>
2023-09-01 21:39:26 +0300

    added path to edit client in urlpatterns

--- send_mail/urls.py
+++ send_mail/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('', ClientListView.as_view(), name='index'),
     path('client/create/', ClientCreateView.as_view(), name='create_client'),
     path('client/view/<int:pk>', ClientDetailView.as_view(), name='view_client'),
-    path('client/')
+    path('client/edit/<int:pk>', ClientUpdateView.as_view(), name='edit_client'),
 ]
