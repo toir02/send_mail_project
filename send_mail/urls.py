a1bc3165e43f8eebbfa0b4b539b2042b88355345
toir02 <wend8765@gmail.com>
2023-09-01 21:41:40 +0300

    added path to delete in urlpatterns

--- send_mail/urls.py
+++ send_mail/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('client/create/', ClientCreateView.as_view(), name='create_client'),
     path('client/view/<int:pk>', ClientDetailView.as_view(), name='view_client'),
     path('client/edit/<int:pk>', ClientUpdateView.as_view(), name='edit_client'),
+    path('client/delete/<int:pk>', ClientDeleteView.as_view(), name='delete_client'),
 ]
