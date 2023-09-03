ce370107e2d4d1fdffccfca0cdcbd4548e41863b
toir02 <wend8765@gmail.com>
2023-09-03 19:19:39 +0300

    added nullable for comment in model client

--- send_mail/models.py
+++ send_mail/models.py
@@ -7,7 +7,7 @@ NULLABLE = {'blank': True,
 class Client(models.Model):
     email = models.EmailField(verbose_name='почта')
     full_name = models.CharField(max_length=150, verbose_name='ФИО')
-    comment = models.TextField(verbose_name='комментарий')
+    comment = models.TextField(verbose_name='комментарий', **NULLABLE)
 
     def __str__(self):
         return self.full_name
