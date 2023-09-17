217bed2559018b211815ef74e709b6a089a86955
toir02 <wend8765@gmail.com>
2023-09-18 01:55:00 +0300

    refactor paths

--- send_mail/urls.py
+++ send_mail/urls.py
@@ -7,9 +7,9 @@ from send_mail.views import *
 app_name = SendMailConfig.name
 
 urlpatterns = [
-    path('', ClientListView.as_view(), name='index'),
-    path('client/create/', ClientCreateView.as_view(), name='create_client'),
-    path('client/view/<int:pk>', ClientDetailView.as_view(), name='view_client'),
-    path('client/edit/<int:pk>', ClientUpdateView.as_view(), name='edit_client'),
-    path('client/delete/<int:pk>', ClientDeleteView.as_view(), name='delete_client'),
+    path('', MailingListView.as_view(), name='index'),
+    path('client/create/', MailCreateView.as_view(), name='create_client'),
+    path('client/view/<int:pk>', MailDetailView.as_view(), name='view_client'),
+    path('client/edit/<int:pk>', MailUpdateView.as_view(), name='edit_client'),
+    path('client/delete/<int:pk>', MailDeleteView.as_view(), name='delete_client'),
 ]
