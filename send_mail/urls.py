5aa1bf10dd04a79558edf187e2e9050f2785abd2
toir02 <wend8765@gmail.com>
2023-09-18 02:01:09 +0300

    rename in paths

--- send_mail/urls.py
+++ send_mail/urls.py
@@ -7,7 +7,7 @@ from send_mail.views import *
 app_name = SendMailConfig.name
 
 urlpatterns = [
-    path('', MailingListView.as_view(), name='index'),
+    path('', MailListView.as_view(), name='index'),
     path('mail/create/', MailCreateView.as_view(), name='create_mail'),
     path('mail/view/<int:pk>', MailDetailView.as_view(), name='view_mail'),
     path('mail/edit/<int:pk>', MailUpdateView.as_view(), name='edit_mail'),
