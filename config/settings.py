3e1873cd098dc0d04373b0f7badc4089003d0359
toir02 <wend8765@gmail.com>
2023-09-20 22:26:45 +0300

    refactor cronjobs

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,7 @@ EMAIL_USE_SSL = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRONJOBS = [
-    ('0 0 * * *', 'mailing.services.run_mail', ['D']),
-    ('0 0 * * 0', 'mailing.services.run_mail', ['W']),
-    ('0 0 1 * *', 'mailing.services.run_mail', ['M']),
+    ('0 0 * * *', 'send_mail.services.run_mail', ['D']),
+    ('0 0 * * 0', 'send_mail.services.run_mail', ['W']),
+    ('0 0 1 * *', 'send_mail.services.run_mail', ['M']),
 ]
