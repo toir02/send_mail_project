3b0a5d25fc61df1d60be1f93d81a82ea1b3fa906
toir02 <wend8765@gmail.com>
2023-09-20 21:57:13 +0300

    added cronjobs

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,9 @@ EMAIL_USE_SSL = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRONJOBS = [
+    ('0 0 * * *', 'mailing.services.run_mail', ['D']),
+    ('0 0 * * 0', 'mailing.services.run_mail', ['W']),
+    ('0 0 1 * *', 'mailing.services.run_mail', ['M']),
+]
