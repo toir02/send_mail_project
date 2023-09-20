605c352019c1dcaaf238429fee790166759dca0f
toir02 <wend8765@gmail.com>
2023-09-20 12:56:07 +0300

    added email settings

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,12 @@ STATICFILES_DIR = (
     BASE_DIR / 'static/',
 )
 
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'noreply@oscarbot.ru'
+EMAIL_HOST_PASSWORD ='AsTSNVv7pun9'
+EMAIL_USE_SSL = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
