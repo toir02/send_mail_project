56a1dc73d38c3d852eff2ed5d9770be72d6e29ad
toir02 <wend8765@gmail.com>
2023-09-21 15:56:21 +0300

    refactor command to create superuser

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,6 +5,9 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(email='admin@admin.ru')
+        user = User.objects.create(email='admin@admin.ru',
+                                   is_staff=True,
+                                   is_superuser=True,
+                                   is_active=True)
         user.set_password('123qwerty123')
         user.save()
