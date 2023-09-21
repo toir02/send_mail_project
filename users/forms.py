f5bd2df97b4126dbac0d67e80d8a4840b49040ac
toir02 <wend8765@gmail.com>
2023-09-21 16:02:38 +0300

    added style form mixin for next forms

---
+++ users/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+from users.models import User
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name != "available":
+                field.widget.attrs['class'] = 'form-control'
