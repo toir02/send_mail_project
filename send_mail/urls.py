c7200d971a634ad93bd9c2c82dedd4b5cc972393
toir02 <wend8765@gmail.com>
2023-09-25 14:12:13 +0300

    added paths

--- send_mail/urls.py
+++ send_mail/urls.py
@@ -12,13 +12,21 @@ urlpatterns = [
     path('settings/view/<int:pk>/', MailDetailView.as_view(), name='view_settings'),
     path('settings/edit/<int:pk>/', MailUpdateView.as_view(), name='edit_settings'),
     path('settings/delete/<int:pk>/', MailDeleteView.as_view(), name='delete_settings'),
+
     path('client/delete/<int:pk>/', ClientDeleteView.as_view(), name='delete_client'),
     path('client/create/', ClientCreateView.as_view(), name='create_client'),
     path('client/edit/<int:pk>/', ClientUpdateView.as_view(), name='edit_client'),
     path('clients/', ClientListView.as_view(), name='clients'),
+
     path('messages/', TextMailListView.as_view(), name='messages'),
     path('message/create/', TextMailCreateView.as_view(), name='create_message'),
     path('message/view/<int:pk>/', TextMailDetailView.as_view(), name='view_message'),
     path('message/edit/<int:pk>/', TextMailUpdateView.as_view(), name='edit_message'),
     path('message/delete/<int:pk>/', TextMailDeleteView.as_view(), name='delete_message'),
+
+    path('mails/', MailingClientListView.as_view(), name='mails'),
+    path('mails/create', MailingClientCreateView.as_view(), name='create_mail'),
+    path('mails/view/<int:pk>', MailingClientDetailView.as_view(), name='view_mail'),
+    path('mails/edit/<int:pk>', MailingClientUpdateView.as_view(), name='edit_mail'),
+    path('mails/delete/<int:pk>', MailingClientDeleteView.as_view(), name='delete_mail'),
 ]
