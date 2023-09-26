f973ec8d0674d5ecc408243cbc55072bbfe85841
toir02 <wend8765@gmail.com>
2023-09-27 01:04:54 +0300

    refactor paths

--- blog/urls.py
+++ blog/urls.py
@@ -6,9 +6,9 @@ from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateV
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('blog/', BlogListView.as_view(), name='blog'),
-    path('blog/view/<int:pk>/', BlogDetailView.as_view(), name='view_blog'),
-    path('blog/create/', BlogCreateView.as_view(), name='create_blog'),
-    path('blog/edit/<int:pk>/', BlogUpdateView.as_view(), name='edit_blog'),
-    path('blog/delete/<int:pk>/', BlogDeleteView.as_view(), name='delete_blog')
+    path('', BlogListView.as_view(), name='blog'),
+    path('view/<int:pk>/', BlogDetailView.as_view(), name='view_blog'),
+    path('create/', BlogCreateView.as_view(), name='create_blog'),
+    path('edit/<int:pk>/', BlogUpdateView.as_view(), name='edit_blog'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete_blog')
 ]
